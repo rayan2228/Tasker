dc9ab8862ed1c1e7c6e25ddbfc97fea0c1046a2f
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2024-01-28 17:00:12 +0600

    all done

--- src/utils/genarateRandomColor/randomColor.js
+++ src/utils/genarateRandomColor/randomColor.js
@@ -1,3 +1,6 @@
 export const randomColor = () => {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
 };
